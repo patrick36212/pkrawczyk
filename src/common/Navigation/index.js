8df3129ddfe58390c2d3b14f3ef42090e2f0b876
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-08 18:44:01 +0100

    navigation component

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ThemeSwitch from "../ThemeSwitch";
+import { NavigaionLink, NavigationWrapper } from "./styled";
 
-const Navigation = () => {
-    return(
-        <></>
+const Navigation = ({ openNav, about, projects, contact }) => {
+    return (
+        <NavigationWrapper openNav={openNav}>
+            <NavigaionLink>{about}</NavigaionLink>
+            <NavigaionLink>{projects}</NavigaionLink>
+            <NavigaionLink>{contact}</NavigaionLink>
+            <ThemeSwitch />
+        </NavigationWrapper>
     )
 };
 
---
+++ src/common/Navigation/styled.js
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+
+export const NavigationWrapper = styled.nav`
+    display: flex;
+    align-items: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+        width: 100%;
+        padding: 10px 0;
+        flex-direction: column;
+        background-color: whitesmoke;
+        transition: all 0.5s ease-in-out; 
+        transform: translateY(-100%);
+
+        ${({ openNav }) => openNav && css`
+            transform: none;
+        `}
+    }
+`;
+
+export const NavigaionLink = styled.a`
+    font-size: 1.5em;
+    padding: 0 10px;
+    cursor: pointer;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+        margin-bottom: 10px;
+    }
+
+    &:hover {
+        border-bottom: 1px solid;
+    }
+
+    &:active {
+        color: teal;
+        border-bottom: 1px solid;
+    }
+`;

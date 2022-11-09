0b61efbc740efdd6876cebabf7505e405fe1aa90
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-09 14:20:44 +0100

    add components to main app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./themes/GlobalStyles";
+import { ThemeProvider } from "styled-components";
 import { lightMode } from "./themes/theme";
 
-import Burger from "../src/common/Burger";
+import Container from "./common/Container";
+import Section from "./common/Section";
+import Header from "./common/Header";
+import Navigation from "./common/Navigation";
+import PageControls from "./common/PageControls";
+import Paragraph from "./common/Paragraph";
 
 const App = () => {
+
   return (
     <ThemeProvider theme={lightMode}>
       <GlobalStyle />
-      <Burger />
+      <Container >
+        <Header
+          navigation={
+            <Navigation
+              about={'about'}
+              projects={'projects'}
+              contact={'contact'}
+            />}
+        />
+        <PageControls />
+        <Section>
+          <Paragraph
+            description={'Page about'}
+          />
+        </Section>
+        <Section>
+          <Paragraph
+            description={`Page projects`}
+          />
+        </Section>
+        <Section>
+          <Paragraph
+            description={`Page contact`}
+          />
+        </Section>
+      </Container>
     </ThemeProvider>
   );
 }

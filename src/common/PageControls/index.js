2d40595f99a436934ffa448e612437371ddf37c1
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-09 14:17:46 +0100

    basic page controls component

--- src/common/PageControls/index.js
+++ src/common/PageControls/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { ControlsWrapper, StyledControl } from "./styled";
 
-const PageControls = () => {
+const PageControls = ({ active, onClick }) => {
+
     return (
         <ControlsWrapper>
-            <StyledControl type="radio" />
-            <StyledControl type="radio" />
-            <StyledControl type="radio" />
+            <StyledControl active={active} onClick={onClick} />
+            <StyledControl active={active} onClick={onClick} />
+            <StyledControl active={active} onClick={onClick} />
         </ControlsWrapper>
-    )
+    );
 };
 
 export default PageControls;
--- src/common/PageControls/styled.js
+++ src/common/PageControls/styled.js
@@ -1,15 +1,30 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 
 export const ControlsWrapper = styled.div`
-    position: fixed;
-    right: 1%;
+    position: absolute;
+    margin: 0 10px;
+    right: 10px;
     top: 50%;
     display: flex;
     flex-direction: column;
 `;
 
-export const StyledControl = styled.input`
+export const StyledControl = styled.span`
     margin: 3px;
     cursor: pointer;
+    height: 15px;
+    width: 15px;
+    background-color: whitesmoke;
+    border: 1px solid gray;
+    border-radius: 50%;
+    transition: background-color 0.6s ease;
+
+    &:hover {
+        background-color: gray;
+    }
+
+    ${({ active }) => active && css`
+        background-color: gray;
+    `}
 `;

48e2c02de3bba818afb69e4750869fac64a6515f
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-19 17:09:22 +0100

    change div -> main

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const FullPageContainer = styled.div`
+export const FullPageContainer = styled.main`
     scroll-snap-type: y mandatory;
     overflow-y: scroll;
     height: 100vh;

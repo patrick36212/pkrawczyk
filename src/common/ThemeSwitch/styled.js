2feb567690a3449d2a58dfd1d31f78bf3400d5d7
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-19 15:40:02 +0100

    change colors & adjust size

--- src/common/ThemeSwitch/styled.js
+++ src/common/ThemeSwitch/styled.js
@@ -8,22 +8,25 @@ export const ThemeSwitchButton = styled.button`
     background-color: transparent;
     border: none;
     display: flex;
-    gap: 10px;
+    gap: 4px;
     align-items: center;
     cursor: pointer;
 `;
 
 export const Info = styled.p`
     margin: 0;
-    font-size: 1.5em;
+    text-transform: uppercase;
+    font-size: 12px;
+    line-height: 15px;
+    font-weight: 700;
     min-width: 115px;
 `;
 
 export const SwitchBody = styled.span`
     width: 51px;
     height: 31px;
-    background-color: gray;
-    border-radius: 30px;
+    background-color: ${({ theme }) => theme.color.mainFont};
+    border-radius: 31px;
     display: flex;
     align-items: center;
 `;
@@ -34,8 +37,9 @@ export const SwitchItem = styled.span`
     margin: 2px;
     width: 27px;
     height: 27px;
-    background-color: teal;
-    transition: all .3s ease-in-out; 
+    background-color: ${({ theme }) => theme.color.mainBackground};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 3px 1px rgba(0, 0, 0, 0.06);
+    transition: all .3s ease-in-out;
 
     ${({ active }) => active && css`
         transform: translateX(20px);   

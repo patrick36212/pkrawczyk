6ea03a770b3f2ccdff242c83228aeac039657b36
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-19 15:18:59 +0100

    add h1 tag and css for him (desktop)

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Burger from "../Burger";
-import { HeaderWrapper } from "./styled";
+import { HeaderWrapper, Title } from "./styled";
 
 const Header = ({ navigation }) => {
     return (
         <HeaderWrapper>
+            <Title>pk development</Title>
             <Burger />
             {navigation}
         </HeaderWrapper>
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.header`
+    padding: 0 16px;
     margin: 10px auto;
     width: 100%;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     background: transparent;
-    position: absolute;
+    position: fixed;
+`;
 
-    @media(max-width: ${({theme}) => theme.breakpoints.medium}px) {
-        margin: 0;
-    }
+export const Title = styled.h1`
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+    text-transform: uppercase;
+    margin: 0;
 `;

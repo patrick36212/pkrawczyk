b162235d293ace3c002c183189889e02ccd13aef
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-20 11:52:52 +0100

    Replace paragraphs with main components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
 import React from "react";
 import GlobalStyle from "./themes/GlobalStyles";
 import { ThemeProvider } from "styled-components";
-import { lightMode } from "./themes/theme";
+import { darkMode } from "./themes/theme";
 
 import Container from "./common/Container";
 import Section from "./common/Section";
 import Header from "./common/Header";
 import Navigation from "./common/Navigation";
-import PageControls from "./common/PageControls";
-import Paragraph from "./common/Paragraph";
+import About from "./features/About";
+import Contact from "./features/Conatct";
 
 const App = () => {
 
   return (
-    <ThemeProvider theme={lightMode}>
+    <ThemeProvider theme={darkMode}>
       <GlobalStyle />
       <Container >
         <Header
-          navigation={
-            <Navigation
-              about={'about'}
-              projects={'projects'}
-              contact={'contact'}
-            />}
+          navigation={<Navigation />}
         />
-        <PageControls />
         <Section>
-          <Paragraph
-            description={'Page about'}
-          />
+          <About />
         </Section>
         <Section>
-          <Paragraph
-            description={`Page projects`}
-          />
+
         </Section>
         <Section>
-          <Paragraph
-            description={`Page contact`}
-          />
+
+        </Section>
+        <Section>
+          <Contact />
         </Section>
       </Container>
     </ThemeProvider>

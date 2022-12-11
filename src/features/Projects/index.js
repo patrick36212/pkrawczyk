537559e661e80f9b2069de99a2278a761d84cef6
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-11 19:14:19 +0100

    create projects component

---
+++ src/features/Projects/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ProjectTile from "../../common/ProjectTile";
+import { ProjectsWrapper } from "./styled";
+
+const Projects = () => {
+
+    return (
+        <ProjectsWrapper>
+            <ProjectTile />
+            <ProjectTile />
+        </ProjectsWrapper>
+    )
+
+}
+
+export default Projects;

f1bc3ec9e2e3ce388b272ceddd3e5fa2d49bbed6
Patryk Krawczyk <patrick36212@gmail.com>
2022-12-12 20:37:43 +0100

    justify code

--- src/themes/GlobalStyles.js
+++ src/themes/GlobalStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     html {
-        box-sizing: border-box;  
+        box-sizing: border-box;
     }
 
     *, ::after, ::before {
@@ -13,8 +13,8 @@ const GlobalStyle = createGlobalStyle`
         font-family: 'Inter', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        background-color: ${({theme}) => theme.color.mainBackground};
-        color: ${({theme}) => theme.color.mainFont};
+        background-color: ${({ theme }) => theme.color.mainBackground};
+        color: ${({ theme }) => theme.color.mainFont};
     }
 `;
 
